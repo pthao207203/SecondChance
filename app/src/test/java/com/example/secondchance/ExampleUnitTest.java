a2bf196136bda378e0ffa52217b7678c5c2c6e35
pthao207203 <149165346+pthao207203@users.noreply.github.com>
2025-10-09 00:03:34 +0700

    [Role Guest] Màn hình đăng ký

--- app/src/test/java/com/example/secondchance/ExampleUnitTest.java
+++ app/src/test/java/com/example/secondchance/ExampleUnitTest.java
@@ -14,4 +14,8 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> d0ef108 ([Role Guest] Màn hình đăng nhập)
